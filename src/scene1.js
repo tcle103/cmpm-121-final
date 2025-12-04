1585f5b5d55f051afe3e383629ab97784b09f4c8
InvinciblePyro <samrexspivey@gmail.com>
2025-12-03 17:17:50 -0800

    add cube function 😱

--- src/scene1.js
+++ src/scene1.js
@@ -61,32 +61,44 @@ export function createScene1(physicsWorld, AmmoLib) {
 
   const holesX = puzzleHoles.map(h => h.position.x);
 
-  for (let i = 0; i < rainbow.length; i++) {
+  function addCube(colorKey) {
     const cubeGeo = new THREE.BoxGeometry(1,1,1);
-    const cubeMat = new THREE.MeshStandardMaterial({ color: rainbow[i] });
+    const cubeMat = new THREE.MeshStandardMaterial({ color: colorKey });
+
     const cube = new THREE.Mesh(cubeGeo, cubeMat);
 
     const x = randomOutsideGoals(-10, 10, holesX);
     const z = Math.random() * 10 - 5;
     cube.position.set(x, 3, z);
 
-    cube.userData.shape = rainbow[i];
+    cube.userData.shape = colorKey;
     scene.add(cube);
 
-    const cubeShape = new AmmoLib.btBoxShape(new AmmoLib.btVector3(0.5, 0.5, 0.5));
-    const cubeTransform = new AmmoLib.btTransform();
-    cubeTransform.setIdentity();
-    cubeTransform.setOrigin(new AmmoLib.btVector3(cube.position.x, cube.position.y, cube.position.z));
-    const cubeMotion = new AmmoLib.btDefaultMotionState(cubeTransform);
-    const localInertia = new AmmoLib.btVector3(0,0,0);
-    cubeShape.calculateLocalInertia(1, localInertia);
-    const rbInfoCube = new AmmoLib.btRigidBodyConstructionInfo(1, cubeMotion, cubeShape, localInertia);
-    const cubeBody = new AmmoLib.btRigidBody(rbInfoCube);
-    physicsWorld.addRigidBody(cubeBody);
+    const shape = new AmmoLib.btBoxShape(new AmmoLib.btVector3(0.5,0.5,0.5));
+    const transform = new AmmoLib.btTransform();
+    transform.setIdentity();
+    transform.setOrigin(new AmmoLib.btVector3(cube.position.x, cube.position.y, cube.position.z));
 
-    cube.userData.physicsBody = cubeBody;
+    const motion = new AmmoLib.btDefaultMotionState(transform);
+    const inertia = new AmmoLib.btVector3(0,0,0);
+    shape.calculateLocalInertia(1, inertia);
+
+    const rbInfo = new AmmoLib.btRigidBodyConstructionInfo(1, motion, shape, inertia);
+    const body = new AmmoLib.btRigidBody(rbInfo);
+
+    physicsWorld.addRigidBody(body);
+    body.setActivationState(4); // keep awake
+
+    cube.userData.physicsBody = body;
     rigidBodies.push(cube);
+
+    return cube;
   }
+  
+  for (let i = 0; i < rainbow.length; i++) {
+    addCube(rainbow[i]);
+  }
+
 
   return { scene, rigidBodies, puzzleHoles };
 }
